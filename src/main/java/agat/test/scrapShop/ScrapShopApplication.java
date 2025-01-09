c6edd3bbf2a5f9b4211cb61ad10faa1d30d0bbbe
Sab1maru <notprogramm@mail.ru>
2025-01-09 21:00:00 +0300

    init commit

---
+++ src/main/java/agat/test/scrapShop/ScrapShopApplication.java
@@ -0,0 +1,13 @@
+package agat.test.scrapShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agat/test/scrapShop/ScrapShopApplicationTests.java
@@ -0,0 +1,13 @@
+package agat.test.scrapShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
